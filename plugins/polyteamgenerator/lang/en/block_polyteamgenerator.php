6072230c3349132e8e445c00943b04f3461f07c0
Amine Kamal <m.amine.kamal@gmail.com>
2020-10-05 19:51:23 -0400

    completed language strings

--- plugins/polyteamgenerator/lang/en/block_polyteamgenerator.php
+++ plugins/polyteamgenerator/lang/en/block_polyteamgenerator.php
@@ -10,6 +10,8 @@ $string['polyteamgenerator:myaddinstance'] = 'Add a new simple HTML block to the
 */
 
 // Generator
+
+// Form
 $string['toggleview'] = 'Toggle View';
 $string['teamsize'] = 'Team Size';
 $string['teamsizepreference'] = 'Team Size';
@@ -23,7 +25,14 @@ $string['generate'] = "Generate";
 
 // Questionnaire
 
+// Form
 $string['send'] = 'Send';
+
+// Sections
+$string['Energy-Direction'] = 'Energy Direction';
+$string['Orientation'] = 'Orientation';
+$string['Information-Collection-Process'] = 'Information Collection Process';
+$string['Decision-Making-Process'] = 'Decision Making Process';
 
 // Questions
 $string['youaremore'] = 'You are more :';
@@ -34,3 +43,45 @@ $string['youworkbetter'] = 'You work better :';
 $string['youpreferthe'] = 'You prefer the :';
 $string['youseeyourselfasmore'] = 'You see yourself as more :';
 $string['youthinkjudgesshouldbe'] = 'You think judges should be :';
+
+// Answers
+$string['Sociable'] = 'Sociable';
+$string['Reserved'] = 'Reserved';
+$string['Expressive'] = 'Expressive';
+$string['Contained'] = 'Contained';
+$string['Groups'] = 'Groups';
+$string['Individuals'] = 'Individuals';
+$string['Listening'] = 'Listening';
+$string['Reading'] = 'Reading';
+$string['Talkative'] = 'Talkative';
+$string['Quiet'] = 'Quiet';
+$string['Systematic'] = 'Systematic';
+$string['Casual'] = 'Casual';
+$string['Planned'] = 'Planned';
+$string['Open-ended'] = 'Open-ended';
+$string['With-pressure'] = 'With pressure';
+$string['Without-pressure'] = 'Without pressure';
+$string['Routine'] = 'Routine';
+$string['Variety'] = 'Variety';
+$string['Methodical'] = 'Methodical';
+$string['Improvisational'] = 'Improvisational';
+$string['Concrete'] = 'Concrete';
+$string['Abstract'] = 'Abstract';
+$string['Fact-finding'] = 'Fact-finding';
+$string['Speculating'] = 'Speculating';
+$string['Practical'] = 'Practical';
+$string['Conceptual'] = 'Conceptual';
+$string['Hands-on'] = 'Hands-on';
+$string['Theoretical'] = 'Theoretical';
+$string['Traditional'] = 'Traditional';
+$string['Novel'] = 'Novel';
+$string['Logic'] = 'Logic';
+$string['Empathy'] = 'Empathy';
+$string['Truthful'] = 'Truthful';
+$string['Tactful'] = 'Tactful';
+$string['Questioning'] = 'Questioning';
+$string['Accommodating'] = 'Accommodating';
+$string['Skeptical'] = 'Skeptical';
+$string['Tolerant'] = 'Tolerant';
+$string['Impartial'] = 'Impartial';
+$string['Merciful'] = 'Merciful';
